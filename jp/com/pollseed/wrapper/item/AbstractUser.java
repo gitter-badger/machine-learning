681d3a1bb4260d57d2239b4767adc36e561df3cd
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 16:11:05 +0900

    Create AbstractUser.java

---
+++ jp/com/pollseed/wrapper/item/AbstractUser.java
@@ -0,0 +1,21 @@
+package jp.com.machine.test.user;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.model.DataModel;
+
+abstract class AbstractUser {
+
+    protected DataModel dataModel;
+    protected UserItemDto dto;
+
+    protected AbstractUser(DataModel dataModel, UserItemDto dto) {
+        if (dataModel == null || dto == null || dto.userMap == null || dto.userMap.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.dataModel = dataModel;
+        this.dto = dto;
+    }
+
+    protected abstract void affinity() throws TasteException;
+
+}
