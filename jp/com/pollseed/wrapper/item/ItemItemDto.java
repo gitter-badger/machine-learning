6c5ca6f8f6f1344c708c70f25f9893705614ea3c
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 16:13:02 +0900

    Create ItemItemDto.java

---
+++ jp/com/pollseed/wrapper/item/ItemItemDto.java
@@ -0,0 +1,34 @@
+package jp.com.machine.test.item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemItemDto {
+
+    public Map<ItemName, ItemDto> itemMap = new HashMap<ItemItemDto.ItemName, ItemDto>();
+
+    public static enum ItemName {
+        TANIMOTO(1, "谷本係数"),
+        CITY_BLOCK(2, "シティブロック距離"),
+        LOG_LIKE(3, "稀にしか起こらない事象"),
+        EUCLIDEAN(4, "ユークリッド距離"),
+        COSINE(5, "コサイン類似度");
+
+        public int num;
+        public String name;
+
+        ItemName(int num, String name) {
+            this.num = num;
+            this.name = name;
+        }
+
+        public static String getName(long val) {
+            for (ItemName _val : ItemName.values()) {
+                if (_val.num == val) {
+                    return _val.name;
+                }
+            }
+            return null;
+        }
+    }
+}
