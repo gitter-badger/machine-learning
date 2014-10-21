e8dcf5885fc2f4a0f76a3f4fc24f130c1c567bfa
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 15:40:02 +0900

    Create EvalItemDto.java

---
+++ jp/com/pollseed/wrapper/eval/EvalItemDto.java
@@ -0,0 +1,31 @@
+package jp.com.machine.test.eval;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EvalItemDto {
+
+    public Map<EvalName, EvalDto> evalMap = new HashMap<EvalItemDto.EvalName, EvalDto>();
+
+    public static enum EvalName {
+        MAE(1, "mean average error"),
+        RMS(2, "root mean squared"), ;
+
+        public int num;
+        public String name;
+
+        EvalName(int num, String name) {
+            this.num = num;
+            this.name = name;
+        }
+
+        public static String getName(long val) {
+            for (EvalName _val : EvalName.values()) {
+                if (_val.num == val) {
+                    return _val.name;
+                }
+            }
+            return null;
+        }
+    }
+}
