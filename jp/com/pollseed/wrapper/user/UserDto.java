1b3b09f9ad2e26fc7587624473e8cec9d9f9e38a
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 15:41:23 +0900

    Create UserDto.java

---
+++ jp/com/pollseed/wrapper/user/UserDto.java
@@ -0,0 +1,13 @@
+package jp.com.machine.test.user;
+
+public class UserDto {
+    public int size;
+    public int userId;
+    public int howMany;
+
+    public UserDto(int size, int userId, int howMany) {
+        this.size = size;
+        this.userId = howMany;
+        this.howMany = howMany;
+    }
+}
