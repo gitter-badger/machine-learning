bcf87df25825a41e8110564c09c73c635a234ed9
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 16:12:32 +0900

    Create ItemDto.java

---
+++ jp/com/pollseed/wrapper/item/ItemDto.java
@@ -0,0 +1,11 @@
+package jp.com.machine.test.item;
+
+public class ItemDto {
+    public long userId;
+    public int howMany;
+
+    public ItemDto(long userId, int howMany) {
+        this.userId = howMany;
+        this.howMany = howMany;
+    }
+}
