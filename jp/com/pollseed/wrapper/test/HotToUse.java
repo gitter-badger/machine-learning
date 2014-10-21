05bb5262641dad8a0ff1dc3dacd99ad49a18198b
Seed <cb.so.devope+0001@gmail.com>
2014-10-21 16:14:35 +0900

    Update HotToUse.java

--- jp/com/pollseed/wrapper/test/HotToUse.java
+++ jp/com/pollseed/wrapper/test/HotToUse.java
@@ -2,54 +2,37 @@ package jp.com.machine.test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-import jp.com.consts.CommonConsts.Critics;
 import jp.com.machine.test.eval.EvalDto;
 import jp.com.machine.test.eval.EvalItemDto;
 import jp.com.machine.test.eval.Evaluator;
 import jp.com.machine.test.eval.EvalItemDto.EvalName;
+import jp.com.machine.test.item.Item;
+import jp.com.machine.test.item.ItemDto;
+import jp.com.machine.test.item.ItemItemDto;
+import jp.com.machine.test.item.ItemItemDto.ItemName;
 import jp.com.machine.test.user.User;
 import jp.com.machine.test.user.UserDto;
 import jp.com.machine.test.user.UserItemDto;
 import jp.com.machine.test.user.UserItemDto.UserName;
-import jp.com.machine.test.MahoutFileParser.Arrange;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.AveragingPreferenceInferrer;
-import org.apache.mahout.cf.taste.impl.similarity.CityBlockSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.UncenteredCosineSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
-import org.apache.mahout.cf.taste.recommender.ItemBasedRecommender;
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
-import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 /**
  * Mahoutのアルゴリズムを使うためのイントロダクションクラス<br>
- * 使い方です。
  * <b>※only UTF-8
  */
 class Introduction {
 
     private static final String DIRECTORY_PATH = "\\machineTest\\jp\\com\\machine\\test\\";
-    private static final String INPUT_PATH = String.format("%sseg.csv", DIRECTORY_PATH);
+    private static final String INPUT_PATH = String.format("%ssegment.csv", DIRECTORY_PATH);
+    private static final String OUTPUT_NAME = String.format("%ssegoutput.csv", DIRECTORY_PATH);
 
     public static void main(String[] args) throws IOException, TasteException {
 
-        DataModel dataModel = new FileDataModel(new File(INPUT_PATH));
+        DataModel dataModel = new FileDataModel(new File(OUTPUT_NAME));
 
         EvalItemDto evalDto = new EvalItemDto();
         evalDto.evalMap.put(EvalName.MAE, new EvalDto(0.78, 1.0));
@@ -64,6 +47,13 @@ class Introduction {
         User user = new User(dataModel, userDto);
         user.affinity();
 
-        itemAffinity(dataModel);
+        ItemItemDto itemDto = new ItemItemDto();
+        itemDto.itemMap.put(ItemName.TANIMOTO, new ItemDto(10, 100));
+        itemDto.itemMap.put(ItemName.CITY_BLOCK, new ItemDto(1, 100));
+        itemDto.itemMap.put(ItemName.LOG_LIKE, new ItemDto(1, 100));
+        itemDto.itemMap.put(ItemName.EUCLIDEAN, new ItemDto(1, 100));
+        itemDto.itemMap.put(ItemName.COSINE, new ItemDto(1, 100));
+        Item item = new Item(dataModel, itemDto);
+        item.affinity();
     }
 }
